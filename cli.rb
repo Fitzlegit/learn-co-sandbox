75a6357fcb858172a4a6f05185664fe542a52bf0
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-08-11 00:49:15 +0000

    Automatically backed up by Learn

---
+++ cli.rb
@@ -0,0 +1,4 @@
+require_relative './config/enviroment.rb'
+
+
+Scraper.new.print_movies
---
+++ test.rb
@@ -0,0 +1,8 @@
+my_array = []
+my_array = Array.new
+
+famous_cats = [ "lil bub", "grumpy cat", "Maru"]
+
+famous_cats << "Puss N'boots"
+
+p famous_cats
